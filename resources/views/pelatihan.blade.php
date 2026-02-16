40ea704648308a03d2088796a1a1999a8d7597a4
Shihab <shihabedogawa@gmail.com>
2026-02-16 11:07:45 +0700

    menghapus file yang di comment

--- resources/views/pelatihan.blade.php
+++ resources/views/pelatihan.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.main')
 @section ('container')
 
-
-
 <h2 class="text-2xl font-bold mb-6">Jadwal Pelatihan PATELKI</h2>
 
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
